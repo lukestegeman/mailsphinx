44b90555fca8dc66bae0cdad416d2f5a094ecf2d
Luke Stegeman <luke.a.stegeman@nasa.gov>
2024-07-03 17:56:48 -0500

    Changed unsubscribe.js to search for result of "pre"

--- docs/unsubscribe.js
+++ docs/unsubscribe.js
@@ -7,19 +7,19 @@ function deleteRow() {
     const email = getQueryParameter('email');
     const baseSite = 'https://lukestegeman.github.io/mailsphinx/'
     if (!email) {
-        document.getElementById('result').innerText = 'Email parameter missing';
+        document.getElementById('pre').innerText = 'Email parameter missing';
         return;
     }
     const scriptURL = 'https://script.google.com/macros/s/AKfycbyfWjUxba5iWALcwK086V0yD_a5NNqPnvpRDMftJdQBR0P1tYPrB9SQuMi-qMg60n4/exec?email=' + encodeURIComponent(email);
     
     fetch(scriptURL).then(response => response.text()).then(result => {
-        document.getElementById('result').innerText = result;
+        document.getElementById('pre').innerText = result;
         if (result === 'Row(s) deleted') {
             window.location.href = baseSite.concat("unsubscribed.html");
         }
     }).catch(error => {
         console.error('Error:', error);
-        document.getElementById('result').innerText = 'Error occurred while deleting the row';
+        document.getElementById('pre').innerText = 'Error occurred while deleting the row';
     });
 }
 
