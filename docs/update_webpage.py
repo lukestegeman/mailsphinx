06ca7c91ad9d6e3b07622c1f1f9d63c6528de9d7
Luke Stegeman <luke.a.stegeman@nasa.gov>
2024-07-04 11:13:04 -0500

    Trying again

--- docs/update_webpage.py
+++ docs/update_webpage.py
@@ -4,7 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials as sac
 import re
 import os
 
-google_script_url = 'https://script.google.com/macros/s/AKfycbwZexaRHlWjA-dRGSrcTnkoBx8ZZQejgYEa6rVtSZJz1IlEXswoK3QKty4a6623QLPL/exec'
+google_script_url = 'https://script.google.com/macros/s/AKfycbw_K7VYWDr3j_bLVfJgrG9UGQNWU_g2znlWDuv7FusUDFl0pc2gHyYA_AeT-3O9ynmM/exec'
 json_keyfile = '../../security/mailsphinx-8010bb19634b.json'
 sheet_id = '1PJlkhI0aimpJH2o7KaN62-Lx0Hp_e-DQoPqi8KjiEC4' 
 
